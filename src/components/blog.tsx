fd3a14b829b01c20fb42f52bfd80e38c47413d34
aldifhrent <aldifahriziilmawan@gmail.com>
2024-09-20 12:24:10 +0700

    fix:blog

--- src/components/blog.tsx
+++ src/components/blog.tsx
@@ -13,15 +13,35 @@ const JostFont = Jost({
   subsets: ["latin"],
 });
 
+const BlogData = [
+  {
+    image: '/blogs/01.svg',
+    type: 'Kitchen Design',
+    title: 'Lets Get Solution For Bulding Construction Work',
+    date: '26 December 2022'
+  },
+  {
+    image: '/blogs/01.svg',
+    type: 'Living Design',
+    title: 'Low Cost Latest Invented Interior Designing Ideas.',
+    date: '22 December 2022'
+  },
+  {
+    image: '/blogs/01.svg',
+    type: 'Interior Design',
+    title: 'Best For Any Office & Business Interior Solution',
+    date: '25 December 2022'
+  },
+]
 const Blog = () => {
   return (
     <Wrapper className='flex flex-col items-center mt-[200px]'>
       <h1 className={cn('text-center text-[40px] md:text-[50px] leading-[125%] tracking-[2%] text-[#292F36]', DMSerif.className)}>Article & News</h1>
       <p className={cn('text-center w-full text-[22px] leading-[150%] tracking-[1%]', JostFont.className)}>It is a long established fact that a reader will be distracted by the of readable content of a page when lookings at its layouts the points of using.</p>
       <div className="flex flex-col lg:flex-row mt-[52px] gap-x-[27px] mx-2">
-        <BlogCard image={'/blogs/01.svg'} type={'Kitchen Design'} title={'Lets Get Solution For Bulding Construction Work'} date={'26 December 2022'}/>
-        <BlogCard image={'/blogs/01.svg'} type='Living Design' title='Low Cost Latest Invented Interior Designing Ideas.' date='22 December 2022'/>
-        <BlogCard image={'/blogs/01.svg'} type='Interior Design' title='Best For Any Office & Business Interior Solution' date='25 December 2022'/>
+       {BlogData.map((item, index) => (
+        <BlogCard key={index} image={item.image} type={item.type} title={item.title} date={item.date}/>
+       ))}
       </div>
     </Wrapper>
   )
